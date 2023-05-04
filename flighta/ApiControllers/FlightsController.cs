737c85d791aa4219305b4fd53b3f7a17c8bded54
Raid7 Raid7 <thmrgb8@gmail.com>
2023-05-04 15:00:32 +0300

    added logging for api controllers Flight and Users

--- flighta/ApiControllers/FlightsController.cs
+++ flighta/ApiControllers/FlightsController.cs
@@ -3,6 +3,7 @@ using Flighta.DataAccess;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace Flighta.ApiControllers
 {
@@ -14,18 +15,22 @@ namespace Flighta.ApiControllers
         public record UpdateFlightDto(int id, string from, string to, int seats, DateTime time, decimal cost);
         private readonly IConfiguration _config;
         private readonly FlightDB _db;
+        private readonly ILogger<FlightsController> _logger;
 
-        public FlightsController(IConfiguration config, FlightDB db)
+        public FlightsController(IConfiguration config, FlightDB db,ILogger<FlightsController> logger)
         {
             _config = config;
             _db = db;
+            _logger = logger;
         }
         // GET: api/<FlightsController>
         [HttpGet]
         [Authorize(Policy = "Admin", AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
         public async Task<List<FlightM>> Get()
         {
-            return await _db.GetFlights();
+            var flights = await _db.GetFlights();
+            _logger.LogInformation($"got {flights.Count} flights through api for {HttpContext.Connection.RemoteIpAddress} at {DateTime.Now}");
+            return flights;
         }
 
         // GET api/<FlightsController>/5
@@ -33,6 +38,7 @@ namespace Flighta.ApiControllers
         [Authorize(Policy = "Admin", AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
         public async Task<FlightM?> Get(int id)
         {
+            _logger.LogInformation($"got flight {id} through api for {HttpContext.Connection.RemoteIpAddress}");
             return await _db.GetFlight(id);
         }
 
@@ -45,6 +51,7 @@ namespace Flighta.ApiControllers
             {
                 FlightM toBeCreated = new FlightM(flight.from, flight.to, flight.seats, flight.time, flight.cost);
                 await _db.AddFLight(toBeCreated);
+                _logger.LogInformation($"Created flight {JsonConvert.SerializeObject(flight)} for {HttpContext.Connection.RemoteIpAddress} at {DateTime.Now}");
                 return Ok(await _db.GetFlightid(flight));
             }
             return BadRequest();
@@ -55,6 +62,7 @@ namespace Flighta.ApiControllers
         [Authorize(Policy = "Admin", AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
         public async Task<IActionResult> Put([FromBody] UpdateFlightDto flight)
         {
+            _logger.LogInformation($"Updating flight {flight.id} for {HttpContext.Connection.RemoteIpAddress} at {DateTime.Now}");
             if (flight != null)
             {
                 try
@@ -62,16 +70,19 @@ namespace Flighta.ApiControllers
                     FlightM toBeUpdated = new(flight.id, flight.from, flight.to, flight.seats, flight.time, flight.cost);
                     if (await _db.UpdateFlight(toBeUpdated))
                     {
-
+                        _logger.LogInformation($"Updated flight {flight.id} for {HttpContext.Connection.RemoteIpAddress} at {DateTime.Now} \n flight data {JsonConvert.SerializeObject(flight)}");
                         return Ok(flight);
                     }
+                    _logger.LogWarning($"Couldn't update flight {flight.id} for {HttpContext.Connection.RemoteIpAddress} at {DateTime.Now} \n request data: {JsonConvert.SerializeObject(HttpContext.Request)}");
                     return BadRequest("no users were affected");
                 }
                 catch (Exception e)
                 {
+                    _logger.LogError(e, $"An error has occured while updating flight {flight.id} at {DateTime.Now} ");
                     return BadRequest($"An error occured while updating the user {flight.id}\n{e.Message}");
                 }
             }
+            _logger.LogWarning($"Couldn't update flight {flight.id} for {HttpContext.Connection.RemoteIpAddress} at {DateTime.Now} \n request data: {JsonConvert.SerializeObject(HttpContext.Request)}");
             return BadRequest("Could not update user");
         }
 
@@ -80,12 +91,15 @@ namespace Flighta.ApiControllers
         [Authorize(Policy = "Admin", AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
         public async Task<IActionResult> DeleteFlight([FromQuery] int id)
         {
+            _logger.LogInformation($"Deleting flight {id} thourgh api for {HttpContext.Connection.RemoteIpAddress}");
             try
             {
+                _logger.LogInformation($"Deleted flight {id} thourgh api for {HttpContext.Connection.RemoteIpAddress}");
                 return Ok(await _db.DeleteFlight(id));
             }
             catch (Exception e)
             {
+                _logger.LogError(e, $"An error has occured while deleting flight {id} at {DateTime.Now} for {HttpContext.Connection.RemoteIpAddress}");
                 return BadRequest($"Unable to delete user: {id}\n{e.Message}");
             }
         }
--- flighta/ApiControllers/UsersController.cs
+++ flighta/ApiControllers/UsersController.cs
@@ -3,6 +3,7 @@ using Flighta.DataAccess;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -17,16 +18,19 @@ namespace Flighta.ApiControllers
         public record CreateUserDto(string username, string password, string email);
         private readonly IConfiguration _config;
         private readonly FlightDB _db;
+        private readonly ILogger<UsersController> _logger;
 
-        public UsersController(IConfiguration config, FlightDB db)
+        public UsersController(IConfiguration config, FlightDB db,ILogger<UsersController> logger)
         {
             _config = config;
             _db = db;
+            _logger = logger;
         }
         // GET: api/<UsersController>
         [HttpGet]
         public async Task<List<User>> Get()
         {
+            _logger.LogInformation($"successfully got users for {HttpContext.Connection.RemoteIpAddress} through api at {DateTime.Now}");
             return await _db.GetUsers();
         }
 
@@ -34,19 +38,24 @@ namespace Flighta.ApiControllers
         [HttpGet("{emailOrUsername}")]
         public async Task<User?> Get(string emailOrUsername)
         {
+            _logger.LogInformation($"Getting userid for {emailOrUsername} through api at {DateTime.Now}");
             return await _db.GetUser(emailOrUsername);
         }
 
         // POST api/<UsersController>
+        //[AllowAnonymous] <--- should we allow anyone to register through the api ? not sure
         [HttpPost]
         public async Task<IActionResult> Post([FromBody] CreateUserDto user)
         {
+            _logger.LogInformation($"Creating user {user} thourgh api");
             if (user != null)
             {
+                _logger.LogInformation($"Created user {user} thorugh api");
                 User toBeCreated = new User(user.username, user.password, user.email);
                 await _db.CreateUser(toBeCreated);
                 return Ok(await _db.GetUserId(user));
             }
+            _logger.LogWarning($"Couldn't create user through api at {DateTime.Now}\n request data {JsonConvert.SerializeObject(HttpContext.Request)}");
             return BadRequest();
         }
 
@@ -54,6 +63,7 @@ namespace Flighta.ApiControllers
         [HttpPut]
         public async Task<IActionResult> Put([FromBody] UpdateUserDto user)
         {
+            _logger.LogInformation($"Updating user {user} thourgh api");
             if (user != null)
             {
                 try
@@ -61,16 +71,18 @@ namespace Flighta.ApiControllers
                     User toBeUpdated = new User(user.userid, user.username, user.password, user.email);
                     if (await _db.UpdateUser(toBeUpdated))
                     {
-
+                        _logger.LogInformation($"Updated user {user} thorugh api");
                         return Ok(user);
                     }
                     return BadRequest("no users were affected");
                 }
                 catch (Exception e)
                 {
+                    _logger.LogError(e,$"an error has occured while updating user {user} thorugh api at {DateTime.Now}");
                     return BadRequest($"An error occured while updating the user {user.userid}\n{e.Message}");
                 }
             }
+            _logger.LogWarning($"invalid user was specified while updating thorugh api \n user data: {user} ");
             return BadRequest("Could not update user");
         }
 
@@ -78,12 +90,15 @@ namespace Flighta.ApiControllers
         [HttpDelete]
         public async Task<IActionResult> DeleteUser([FromQuery] int id)
         {
+            _logger.LogInformation($"Deleting user {id} thourgh api");
             try
             {
+                _logger.LogInformation($"Deleted user {id} thourgh api");
                 return Ok(await _db.DeleteUser(id));
             }
             catch (Exception e)
             {
+                _logger.LogError(e, $"An error has occured while deleting user through api at {DateTime.Now}");
                 return BadRequest($"Unable to delete user: {id}\n{e.Message}");
             }
         }
