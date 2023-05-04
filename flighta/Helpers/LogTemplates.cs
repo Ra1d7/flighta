91b943df539541c77a06a70cfadf0f5c9a5cdece
Raid7 Raid7 <thmrgb8@gmail.com>
2023-05-04 15:25:49 +0300

    fixed logging template

--- flighta/Helpers/LogTemplates.cs
+++ flighta/Helpers/LogTemplates.cs
@@ -2,7 +2,7 @@
 {
     public static partial class LogTemplates
     {
-        [LoggerMessage(20,LogLevel.Information,"Conroller {controllerName}/{path} requested at {date}. by {user}}")]
+        [LoggerMessage(20,LogLevel.Information,"Conroller {controllerName}/{path} requested at {date}. by {user}")]
         public static partial void MvcControllerLog(this ILogger logger, string controllerName,string path, DateTime date,string user = "anonymous");
         [LoggerMessage(19,LogLevel.Information,"Feedback Email sent at {date} by {fullName} with email {email}")]
         public static partial void EmailSendLog(this ILogger logger, string fullName, string email, DateTime date);
